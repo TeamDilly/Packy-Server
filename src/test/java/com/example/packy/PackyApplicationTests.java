a363eb8c97908b53396f0971b8ebeb1a59a79d2d
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-08 01:09:00 +0900

    initial commit

---
+++ src/test/java/com/example/packy/PackyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.packy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
