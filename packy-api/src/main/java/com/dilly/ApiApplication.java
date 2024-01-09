f0fc90b3ea08394904480c1c2a9d10d9999b0a02
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-09 14:33:58 +0900

    fix: scanBasePackages 옵션 제거

--- packy-api/src/main/java/com/dilly/ApiApplication.java
+++ packy-api/src/main/java/com/dilly/ApiApplication.java
@@ -3,7 +3,7 @@ package com.dilly;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.dilly")
+@SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
