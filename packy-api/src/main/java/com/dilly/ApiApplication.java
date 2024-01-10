c2fd2e18af0434065c603339a61a6ca3c86a2660
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-10 14:35:06 +0900

    feat: 로컬 MySQL 환경 구축

--- packy-api/src/main/java/com/dilly/ApiApplication.java
+++ packy-api/src/main/java/com/dilly/ApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.config.name", "application-api, application-domain");
         SpringApplication.run(ApiApplication.class, args);
     }
 }
