4ca95ac23a46d7616491003b032f42f1e6217308
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-18 00:33:50 +0900

    feat: Application의 timezone을 Asia/Seoul로 설정

--- packy-api/src/main/java/com/dilly/ApiApplication.java
+++ packy-api/src/main/java/com/dilly/ApiApplication.java
@@ -1,10 +1,19 @@
 package com.dilly;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ApiApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		System.setProperty("spring.config.name", "application-api, application-domain");
