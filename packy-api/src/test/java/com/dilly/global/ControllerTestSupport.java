c41050d3fd4ba6bd4d32b42889ce2209fd0f81c5
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-18 00:52:37 +0900

    fix: Spring Rest Docs 관련 코드 제거

--- packy-api/src/test/java/com/dilly/global/ControllerTestSupport.java
+++ packy-api/src/test/java/com/dilly/global/ControllerTestSupport.java
@@ -1,23 +1,19 @@
 package com.dilly.global;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.dilly.admin.api.AdminController;
 import com.dilly.admin.application.AdminService;
-import com.dilly.global.exception.ErrorCodeController;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(
 	controllers = {
-		AdminController.class,
-		ErrorCodeController.class
+		AdminController.class
 	}
 )
-@AutoConfigureRestDocs
 public abstract class ControllerTestSupport {
 	static {
 		System.setProperty("spring.config.name", "application-test");
