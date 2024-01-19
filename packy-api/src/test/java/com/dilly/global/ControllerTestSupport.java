683ad31615bc0242e940c915fb57a4175508d219
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-19 16:48:48 +0900

    test: 선물박스 보내기 API Happy Case에 대한 Controller 테스트 코드 작성

--- packy-api/src/test/java/com/dilly/global/ControllerTestSupport.java
+++ packy-api/src/test/java/com/dilly/global/ControllerTestSupport.java
@@ -7,17 +7,22 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import com.dilly.admin.api.AdminController;
 import com.dilly.admin.application.AdminService;
+import com.dilly.gift.api.GiftController;
+import com.dilly.gift.application.GiftService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(
 	controllers = {
-		AdminController.class
+		AdminController.class,
+		GiftController.class
 	}
 )
 public abstract class ControllerTestSupport {
 	static {
 		System.setProperty("spring.config.name", "application-test");
 	}
+
+	protected final String BASE_URL = "/api/v1";
 
 	@Autowired
 	protected MockMvc mockMvc;
@@ -27,4 +32,7 @@ public abstract class ControllerTestSupport {
 
 	@MockBean
 	protected AdminService adminService;
+
+	@MockBean
+	protected GiftService giftService;
 }
