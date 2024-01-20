7e9f48bd5e1ca47ca958489ae832f4167dd23297
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-21 01:46:27 +0900

    fix: 로컬에서 스웨거를 사용하기 위한 OpenAPIDefinition 추가

--- packy-api/src/main/java/com/dilly/ApiApplication.java
+++ packy-api/src/main/java/com/dilly/ApiApplication.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.annotation.PostConstruct;
 
 @OpenAPIDefinition(servers = {
+	@Server(url = "http://localhost:8081", description = "Local Server URL"),
 	@Server(url = "https://dev.packyforyou.shop", description = "Dev Server URL")
 })
 @SpringBootApplication
