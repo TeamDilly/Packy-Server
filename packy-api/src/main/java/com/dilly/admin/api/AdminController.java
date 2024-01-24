a024658a54cc87c498a5111af937ddfaa4dbd80c
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-25 03:26:05 +0900

    feat: 스티커 조회 기능 구현

--- packy-api/src/main/java/com/dilly/admin/api/AdminController.java
+++ packy-api/src/main/java/com/dilly/admin/api/AdminController.java
@@ -1,20 +1,23 @@
 package com.dilly.admin.api;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.dilly.admin.application.AdminService;
 import com.dilly.admin.dto.response.BoxImgResponse;
 import com.dilly.admin.dto.response.ImgResponse;
 import com.dilly.admin.dto.response.MusicResponse;
 import com.dilly.global.response.DataResponseDto;
-
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "관리자 API")
 @RestController
@@ -22,37 +25,47 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AdminController {
 
-	private final AdminService adminService;
+    private final AdminService adminService;
 
-	@Operation(summary = "서버 상태 체크")
-	@GetMapping("/health")
-	public DataResponseDto<String> healthCheck() {
-		String activeProfile = System.getProperty("spring.profiles.active");
+    @Operation(summary = "서버 상태 체크")
+    @GetMapping("/health")
+    public DataResponseDto<String> healthCheck() {
+        String activeProfile = System.getProperty("spring.profiles.active");
 
-		return DataResponseDto.from("This is " + activeProfile + " server!");
-	}
+        return DataResponseDto.from("This is " + activeProfile + " server!");
+    }
 
-	@Operation(summary = "프로필 이미지 조회")
-	@GetMapping("/design/profiles")
-	public DataResponseDto<List<ImgResponse>> getProfiles() {
-		return DataResponseDto.from(adminService.getProfiles());
-	}
+    @Operation(summary = "프로필 이미지 조회")
+    @GetMapping("/design/profiles")
+    public DataResponseDto<List<ImgResponse>> getProfiles() {
+        return DataResponseDto.from(adminService.getProfiles());
+    }
 
-	@Operation(summary = "박스 디자인 조회")
-	@GetMapping("/design/boxes")
-	public DataResponseDto<List<BoxImgResponse>> getBoxes() {
-		return DataResponseDto.from(adminService.getBoxes());
-	}
+    @Operation(summary = "박스 디자인 조회")
+    @GetMapping("/design/boxes")
+    public DataResponseDto<List<BoxImgResponse>> getBoxes() {
+        return DataResponseDto.from(adminService.getBoxes());
+    }
 
-	@Operation(summary = "편지 봉투 디자인 조회")
-	@GetMapping("/design/envelopes")
-	public DataResponseDto<List<ImgResponse>> getEnvelopes() {
-		return DataResponseDto.from(adminService.getEnvelopes());
-	}
+    @Operation(summary = "편지 봉투 디자인 조회")
+    @GetMapping("/design/envelopes")
+    public DataResponseDto<List<ImgResponse>> getEnvelopes() {
+        return DataResponseDto.from(adminService.getEnvelopes());
+    }
 
-	@Operation(summary = "패키 추천 음악 조회")
-	@GetMapping("/music")
-	public DataResponseDto<List<MusicResponse>> getMusics() {
-		return DataResponseDto.from(adminService.getMusics());
-	}
+    @Operation(summary = "패키 추천 음악 조회")
+    @GetMapping("/music")
+    public DataResponseDto<List<MusicResponse>> getMusics() {
+        return DataResponseDto.from(adminService.getMusics());
+    }
+
+    @Operation(summary = "스티커 조회")
+    @GetMapping("/design/stickers")
+    public DataResponseDto<Slice<ImgResponse>> getStickers(
+        @Parameter(hidden = true) @PageableDefault(size = 10) Pageable pageable,
+        @Schema(description = "마지막 스티커 ID", example = "10")
+        @RequestParam(value = "lastStickerId", required = false)
+        Long lastStickerId) {
+        return DataResponseDto.from(adminService.getStickers(lastStickerId, pageable));
+    }
 }
