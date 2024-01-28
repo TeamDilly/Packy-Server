3e304f561d410008af0a6c3ace1eef21b9c9d0bf
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-01-28 16:17:06 +0900

    fix: 선물박스 만들기 API에서 선물이 없을 경우 Gift를 null로 저장 (#88)
    * fix: gift가 null일 경우 Gift 객체를 null로 저장
    
    * test: 선물이 없을 경우 선물박스 만들기 API Controller, Service 테스트 코드 작성

--- packy-api/src/test/java/com/dilly/gift/application/GiftServiceTest.java
+++ packy-api/src/test/java/com/dilly/gift/application/GiftServiceTest.java
@@ -15,70 +15,120 @@ import com.dilly.global.IntegrationTestSupport;
 import com.dilly.global.WithCustomMockUser;
 import com.dilly.global.utils.SecurityUtil;
 import com.dilly.member.Member;
+import java.util.Collection;
 import java.util.List;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
 class GiftServiceTest extends IntegrationTestSupport {
 
-    @DisplayName("선물 박스를 만든다.")
-    @Test
+    @DisplayName("선물박스 만들기 시나리오")
+    @TestFactory
     @WithCustomMockUser
-    void createGiftBox() {
+    Collection<DynamicTest> createGiftBox() {
         // given
         Long memberId = SecurityUtil.getMemberId();
         Member member = memberRepository.findById(memberId).orElseThrow();
 
-        GiftBoxRequest giftBoxRequest = GiftBoxRequest.builder()
-            .name("test")
-            .senderName("sender")
-            .receiverName("receiver")
-            .boxId(1L)
-            .envelopeId(1L)
-            .letterContent("This is letter content.")
-            .youtubeUrl("www.youtube.com")
-            .photos(List.of(
-                PhotoRequest.builder().photoUrl("www.test1.com").description("description1")
-                    .sequence(1).build(),
-                PhotoRequest.builder().photoUrl("www.test2.com").description("description2")
-                    .sequence(2).build()
-            ))
-            .stickers(List.of(
-                StickerRequest.builder().id(1L).location(1).build(),
-                StickerRequest.builder().id(2L).location(2).build())
-            )
-            .gift(GiftRequest.builder()
-                .type("photo")
-                .url("www.naver.com")
+        List<PhotoRequest> photoRequests = List.of(
+            PhotoRequest.builder()
+                .photoUrl("www.test1.com").description("description1").sequence(1)
+                .build(),
+            PhotoRequest.builder()
+                .photoUrl("www.test2.com").description("description2").sequence(2)
                 .build()
-            )
-            .build();
+        );
+        List<StickerRequest> stickers = List.of(
+            StickerRequest.builder().id(1L).location(1).build(),
+            StickerRequest.builder().id(2L).location(2).build());
 
-        // when
-        GiftBoxResponse giftBoxResponse = giftService.createGiftBox(giftBoxRequest);
-        GiftBox giftBox = giftBoxRepository.findTopByOrderByIdDesc();
-        List<Photo> photos = photoRepository.findAllByGiftBox(giftBox);
+        return List.of(
+            DynamicTest.dynamicTest("선물이 있을 경우", () -> {
+                //given
+                GiftBoxRequest giftBoxRequest = GiftBoxRequest.builder()
+                    .name("test")
+                    .senderName("sender")
+                    .receiverName("receiver")
+                    .boxId(1L)
+                    .envelopeId(1L)
+                    .letterContent("This is letter content.")
+                    .youtubeUrl("www.youtube.com")
+                    .photos(photoRequests)
+                    .stickers(stickers)
+                    .gift(GiftRequest.builder()
+                        .type("photo")
+                        .url("www.naver.com")
+                        .build()
+                    )
+                    .build();
 
-        // then
-        assertThat(giftBox.getBox().getId()).isEqualTo(giftBoxRequest.boxId());
-        assertThat(giftBox.getName()).isEqualTo(giftBoxRequest.name());
-        assertThat(giftBox.getLetter().getEnvelope().getId()).isEqualTo(
-            giftBoxRequest.envelopeId());
-        assertThat(giftBox.getLetter().getContent()).isEqualTo(giftBoxRequest.letterContent());
-        assertThat(giftBox.getYoutubeUrl()).isEqualTo(giftBoxRequest.youtubeUrl());
-        assertThat(giftBox.getGift().getGiftType().name()).isEqualTo(
-            giftBoxRequest.gift().type().toUpperCase());
-        assertThat(giftBox.getGift().getGiftUrl()).isEqualTo(giftBoxRequest.gift().url());
-        assertThat(photos).hasSize(2)
-            .extracting("imgUrl", "description", "sequence")
-            .contains(tuple("www.test1.com", "description1", 1),
-                tuple("www.test2.com", "description2", 2));
-        assertThat(giftBoxResponse.id()).isEqualTo(giftBox.getId());
-        assertTrue(isValidUUID(giftBoxResponse.uuid()));
+                // when
+                GiftBoxResponse giftBoxResponse = giftService.createGiftBox(giftBoxRequest);
+                GiftBox giftBox = giftBoxRepository.findTopByOrderByIdDesc();
+                List<Photo> photos = photoRepository.findAllByGiftBox(giftBox);
+
+                // then
+                assertThat(giftBox.getBox().getId()).isEqualTo(giftBoxRequest.boxId());
+                assertThat(giftBox.getName()).isEqualTo(giftBoxRequest.name());
+                assertThat(giftBox.getLetter().getEnvelope().getId()).isEqualTo(
+                    giftBoxRequest.envelopeId());
+                assertThat(giftBox.getLetter().getContent()).isEqualTo(
+                    giftBoxRequest.letterContent());
+                assertThat(giftBox.getYoutubeUrl()).isEqualTo(giftBoxRequest.youtubeUrl());
+                assertThat(giftBox.getGift().getGiftType().name()).isEqualTo(
+                    giftBoxRequest.gift().type().toUpperCase());
+                assertThat(giftBox.getGift().getGiftUrl()).isEqualTo(giftBoxRequest.gift().url());
+                assertThat(photos).hasSize(2)
+                    .extracting("imgUrl", "description", "sequence")
+                    .contains(tuple("www.test1.com", "description1", 1),
+                        tuple("www.test2.com", "description2", 2));
+
+                assertThat(giftBoxResponse.id()).isEqualTo(giftBox.getId());
+                assertTrue(isValidUUID(giftBoxResponse.uuid()));
+            }),
+            DynamicTest.dynamicTest("선물이 없을 경우", () -> {
+                //given
+                GiftBoxRequest giftBoxRequest = GiftBoxRequest.builder()
+                    .name("test")
+                    .senderName("sender")
+                    .receiverName("receiver")
+                    .boxId(1L)
+                    .envelopeId(1L)
+                    .letterContent("This is letter content.")
+                    .youtubeUrl("www.youtube.com")
+                    .photos(photoRequests)
+                    .stickers(stickers)
+                    .build();
+
+                // when
+                GiftBoxResponse giftBoxResponse = giftService.createGiftBox(giftBoxRequest);
+                GiftBox giftBox = giftBoxRepository.findTopByOrderByIdDesc();
+                List<Photo> photos = photoRepository.findAllByGiftBox(giftBox);
+
+                // then
+                assertThat(giftBox.getBox().getId()).isEqualTo(giftBoxRequest.boxId());
+                assertThat(giftBox.getName()).isEqualTo(giftBoxRequest.name());
+                assertThat(giftBox.getLetter().getEnvelope().getId()).isEqualTo(
+                    giftBoxRequest.envelopeId());
+                assertThat(giftBox.getLetter().getContent()).isEqualTo(
+                    giftBoxRequest.letterContent());
+                assertThat(giftBox.getYoutubeUrl()).isEqualTo(giftBoxRequest.youtubeUrl());
+                assertThat(giftBox.getGift()).isNull();
+                assertThat(photos).hasSize(2)
+                    .extracting("imgUrl", "description", "sequence")
+                    .contains(tuple("www.test1.com", "description1", 1),
+                        tuple("www.test2.com", "description2", 2));
+
+                assertThat(giftBoxResponse.id()).isEqualTo(giftBox.getId());
+                assertTrue(isValidUUID(giftBoxResponse.uuid()));
+            })
+        );
     }
 
-    public static boolean isValidUUID(String value) {
+
+    private boolean isValidUUID(String value) {
         final Pattern uuidPattern =
             Pattern.compile(
                 "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
