bbb911e3ad533db0d1713bf84528d2e212fc940c
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-01-28 16:11:58 +0900

    test: 선물이 없을 경우 선물박스 만들기 API Controller, Service 테스트 코드 작성

--- packy-api/src/test/java/com/dilly/gift/api/GiftControllerTest.java
+++ packy-api/src/test/java/com/dilly/gift/api/GiftControllerTest.java
@@ -14,58 +14,99 @@ import com.dilly.gift.dto.request.StickerRequest;
 import com.dilly.gift.dto.response.GiftBoxResponse;
 import com.dilly.global.ControllerTestSupport;
 import com.dilly.global.WithCustomMockUser;
+import java.util.Collection;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.http.MediaType;
 
 class GiftControllerTest extends ControllerTestSupport {
 
-    @DisplayName("선물박스를 만든다.")
-    @Test
+    @DisplayName("선물박스 만들기 시나리오")
+    @TestFactory
     @WithCustomMockUser
-    void createGiftBox() throws Exception {
+    Collection<DynamicTest> createGiftBox() {
         // given
-        GiftBoxRequest giftBoxRequest = GiftBoxRequest.builder()
-            .name("test")
-            .senderName("sender")
-            .receiverName("receiver")
-            .boxId(1L)
-            .envelopeId(1L)
-            .letterContent("This is letter content.")
-            .youtubeUrl("www.youtube.com")
-            .photos(List.of(
-                PhotoRequest.builder().photoUrl("www.test1.com").description("description1")
-                    .build(),
-                PhotoRequest.builder().photoUrl("www.test2.com").description("description2").build()
-            ))
-            .stickers(List.of(
-                StickerRequest.builder().id(1L).location(1).build(),
-                StickerRequest.builder().id(2L).location(2).build())
-            )
-            .gift(GiftRequest.builder()
-                .type("photo")
-                .url("www.naver.com")
+        List<PhotoRequest> photos = List.of(
+            PhotoRequest.builder()
+                .photoUrl("www.test1.com").description("description1").sequence(1)
+                .build(),
+            PhotoRequest.builder()
+                .photoUrl("www.test2.com").description("description2").sequence(2)
                 .build()
-            )
-            .build();
+        );
+        List<StickerRequest> stickers = List.of(
+            StickerRequest.builder().id(1L).location(1).build(),
+            StickerRequest.builder().id(2L).location(2).build());
         GiftBoxResponse giftBoxResponse = GiftBoxResponse.builder()
             .id(1L)
             .uuid("550e8400-e29b-41d4-a716-446655440000")
             .build();
 
-        given(giftService.createGiftBox(giftBoxRequest)).willReturn(giftBoxResponse);
+        return List.of(
+            DynamicTest.dynamicTest("선물이 있을 경우", () -> {
+                //given
+                GiftBoxRequest giftBoxRequest = GiftBoxRequest.builder()
+                    .name("test")
+                    .senderName("sender")
+                    .receiverName("receiver")
+                    .boxId(1L)
+                    .envelopeId(1L)
+                    .letterContent("This is letter content.")
+                    .youtubeUrl("www.youtube.com")
+                    .photos(photos)
+                    .stickers(stickers)
+                    .gift(GiftRequest.builder()
+                        .type("photo")
+                        .url("www.naver.com")
+                        .build()
+                    )
+                    .build();
 
-        // when // then
-        mockMvc.perform(
-                post(baseUrl + "/giftbox")
-                    .with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(giftBoxRequest))
-            )
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.data.id").isNumber())
-            .andExpect(jsonPath("$.data.uuid").isString());
+                given(giftService.createGiftBox(giftBoxRequest)).willReturn(giftBoxResponse);
+
+                // when // then
+                mockMvc.perform(
+                        post(baseUrl + "/giftbox")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(giftBoxRequest))
+                    )
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.id").isNumber())
+                    .andExpect(jsonPath("$.data.uuid").isString());
+            }),
+            DynamicTest.dynamicTest("선물이 없을 경우", () -> {
+                //given
+                GiftBoxRequest giftBoxRequest = GiftBoxRequest.builder()
+                    .name("test")
+                    .senderName("sender")
+                    .receiverName("receiver")
+                    .boxId(1L)
+                    .envelopeId(1L)
+                    .letterContent("This is letter content.")
+                    .youtubeUrl("www.youtube.com")
+                    .photos(photos)
+                    .stickers(stickers)
+                    .gift(null)
+                    .build();
+
+                given(giftService.createGiftBox(giftBoxRequest)).willReturn(giftBoxResponse);
+
+                // when // then
+                mockMvc.perform(
+                        post(baseUrl + "/giftbox")
+                            .with(csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(giftBoxRequest))
+                    )
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.data.id").isNumber())
+                    .andExpect(jsonPath("$.data.uuid").isString());
+            })
+        );
     }
 }
