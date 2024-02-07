3f835c96176f629bad2f96dc01f39ea4cf7f3f47
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-08 00:19:59 +0900

    fix: 주고받은 선물박스에 받는 사람 이름 추가 및 Swagger 예시 추가

--- packy-api/src/main/java/com/dilly/gift/dto/response/GiftBoxesResponse.java
+++ packy-api/src/main/java/com/dilly/gift/dto/response/GiftBoxesResponse.java
@@ -3,15 +3,23 @@ package com.dilly.gift.dto.response;
 import com.dilly.gift.domain.GiftBox;
 import com.dilly.gift.domain.Receiver;
 import com.dilly.member.domain.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record GiftBoxesResponse(
+    @Schema(example = "1")
     Long id,
+    @Schema(example = "보내는사람")
     String sender,
+    @Schema(example = "받는사람")
+    String receiver,
+    @Schema(example = "최고의선물박스")
     String name,
+    @Schema(example = "2024-01-01T00:00:00")
     LocalDateTime giftBoxDate,
+    @Schema(example = "www.example.com")
     String boxNormal
 ) {
 
@@ -19,6 +27,7 @@ public record GiftBoxesResponse(
         return GiftBoxesResponse.builder()
             .id(giftBox.getId())
             .sender(giftBox.getSenderName())
+            .receiver(giftBox.getReceiverName())
             .name(giftBox.getName())
             .giftBoxDate(giftBox.getCreatedAt())
             .boxNormal(giftBox.getBox().getNormalImgUrl())
@@ -29,6 +38,7 @@ public record GiftBoxesResponse(
         return GiftBoxesResponse.builder()
             .id(receiver.getGiftBox().getId())
             .sender(receiver.getGiftBox().getSenderName())
+            .receiver(receiver.getGiftBox().getReceiverName())
             .name(receiver.getGiftBox().getName())
             .giftBoxDate(receiver.getCreatedAt())
             .boxNormal(receiver.getGiftBox().getBox().getNormalImgUrl())
@@ -46,6 +56,7 @@ public record GiftBoxesResponse(
         return GiftBoxesResponse.builder()
             .id(giftBox.getId())
             .sender(giftBox.getSenderName())
+            .receiver(giftBox.getReceiverName())
             .name(giftBox.getName())
             .giftBoxDate(giftBoxDate)
             .boxNormal(giftBox.getBox().getNormalImgUrl())
