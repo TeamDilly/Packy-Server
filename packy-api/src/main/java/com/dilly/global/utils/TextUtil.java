358af43e08a90bc180adbc222a72881683e6c4f1
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-16 18:13:39 +0900

    refactor: TextUtil 클래스에 private 생성자 추가

--- packy-api/src/main/java/com/dilly/global/utils/TextUtil.java
+++ packy-api/src/main/java/com/dilly/global/utils/TextUtil.java
@@ -5,6 +5,9 @@ import java.util.regex.Pattern;
 
 public class TextUtil {
 
+    private TextUtil() {
+    }
+    
     private static final Pattern graphemePattern = Pattern.compile("\\X");
 
     public static int countGraphemeClusters(String text) {
