74a3121ac0fea4149097460598ab9fbc4693a4b2
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-02-19 00:08:44 +0900

    fix: 글자 수 유효성 검증을 커스텀 어노테이션을 사용하도록 변경 (#163)
    * fix: 글자 수 유효성 검증을 커스텀 어노테이션을 사용하도록 변경
    
    * deploy: deploy 패키지 생성 시 ebextensions 추가

--- packy-api/src/main/java/com/dilly/gift/dto/request/PhotoRequest.java
+++ packy-api/src/main/java/com/dilly/gift/dto/request/PhotoRequest.java
@@ -1,5 +1,6 @@
 package com.dilly.gift.dto.request;
 
+import com.dilly.global.utils.validator.CustomSize;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
@@ -8,6 +9,7 @@ public record PhotoRequest(
 	@Schema(example = "www.test.com")
 	String photoUrl,
 	@Schema(example = "우리 같이 놀러갔던 날 :)")
+	@CustomSize(max = 20, message = "사진 속 추억은 20자 이하로 입력해주세요.")
 	String description,
 	@Schema(example = "1")
 	Integer sequence
