69f60898ffbe504e80514eb572b1e0155fd4714e
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-20 01:00:00 +0900

    test: 나의 프로필 수정 API Controller, Service 테스트 코드 작성

--- packy-api/src/test/java/com/dilly/mypage/api/MyPageControllerTest.java
+++ packy-api/src/test/java/com/dilly/mypage/api/MyPageControllerTest.java
@@ -1,16 +1,21 @@
 package com.dilly.mypage.api;
 
 import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dilly.global.ControllerTestSupport;
 import com.dilly.global.WithCustomMockUser;
+import com.dilly.mypage.dto.request.ProfileRequest;
 import com.dilly.mypage.dto.response.ProfileResponse;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 class MyPageControllerTest extends ControllerTestSupport {
 
@@ -39,5 +44,100 @@ class MyPageControllerTest extends ControllerTestSupport {
             .andExpect(jsonPath("$.data.provider").value("kakao"))
             .andExpect(jsonPath("$.data.nickname").value("테스트"))
             .andExpect(jsonPath("$.data.imgUrl").value("www.test.com"));
+    }
+
+    @Nested
+    @DisplayName("나의 프로필을 수정한다.")
+    class updateProfile {
+
+        private final String newNickname = "닉네임변경";
+
+        @DisplayName("닉네임과 프로필 이미지를 수정한다.")
+        @Test
+        @WithCustomMockUser
+        void updateNicknameAndProfileImage() throws Exception {
+            // given
+            ProfileRequest profileRequest = ProfileRequest.builder()
+                .nickname(newNickname)
+                .profileImg(2L)
+                .build();
+
+            given(myPageService.updateProfile(profileRequest)).willReturn(ProfileResponse.builder()
+                .id(1L)
+                .provider("test")
+                .nickname(newNickname)
+                .imgUrl("www.example2.com")
+                .build());
+
+            // when // then
+            mockMvc.perform(
+                    patch("/api/v1/my-page/profile")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(profileRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.nickname").value(newNickname))
+                .andExpect(jsonPath("$.data.imgUrl").value("www.example2.com"));
+        }
+
+        @DisplayName("닉네임만 수정한다.")
+        @Test
+        @WithCustomMockUser
+        void updateNickname() throws Exception {
+            // given
+            ProfileRequest profileRequest = ProfileRequest.builder()
+                .nickname(newNickname)
+                .build();
+
+            given(myPageService.updateProfile(profileRequest)).willReturn(ProfileResponse.builder()
+                .id(1L)
+                .provider("test")
+                .nickname(newNickname)
+                .imgUrl("www.example1.com")
+                .build());
+
+            // when // then
+            mockMvc.perform(
+                    patch("/api/v1/my-page/profile")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(profileRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.nickname").value(newNickname))
+                .andExpect(jsonPath("$.data.imgUrl").value("www.example1.com"));
+        }
+
+        @DisplayName("프로필 이미지만 수정한다.")
+        @Test
+        @WithCustomMockUser
+        void updateProfileImage() throws Exception {
+            // given
+            ProfileRequest profileRequest = ProfileRequest.builder()
+                .profileImg(2L)
+                .build();
+
+            given(myPageService.updateProfile(profileRequest)).willReturn(ProfileResponse.builder()
+                .id(1L)
+                .provider("test")
+                .nickname("1번유저")
+                .imgUrl("www.example2.com")
+                .build());
+
+            // when // then
+            mockMvc.perform(
+                    patch("/api/v1/my-page/profile")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(profileRequest))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.nickname").value("1번유저"))
+                .andExpect(jsonPath("$.data.imgUrl").value("www.example2.com"));
+        }
     }
 }
--- packy-api/src/test/java/com/dilly/mypage/application/MyPageServiceTest.java
+++ packy-api/src/test/java/com/dilly/mypage/application/MyPageServiceTest.java
@@ -6,8 +6,10 @@ import com.dilly.global.IntegrationTestSupport;
 import com.dilly.global.WithCustomMockUser;
 import com.dilly.global.utils.SecurityUtil;
 import com.dilly.member.domain.Member;
+import com.dilly.mypage.dto.request.ProfileRequest;
 import com.dilly.mypage.dto.response.ProfileResponse;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class MyPageServiceTest extends IntegrationTestSupport {
@@ -28,5 +30,64 @@ class MyPageServiceTest extends IntegrationTestSupport {
         assertThat(response.provider()).isEqualTo(member.getProvider().toString().toLowerCase());
         assertThat(response.nickname()).isEqualTo(member.getNickname());
         assertThat(response.imgUrl()).isEqualTo(member.getProfileImg().getImgUrl());
+    }
+
+    @Nested
+    @DisplayName("나의 프로필을 수정한다.")
+    class updateProfile {
+
+        private final String newNickname = "닉네임변경";
+
+        @DisplayName("닉네임과 프로필 이미지를 수정한다.")
+        @Test
+        @WithCustomMockUser
+        void updateNicknameAndProfileImage() {
+            // given
+            ProfileRequest profileRequest = ProfileRequest.builder()
+                .nickname(newNickname)
+                .profileImg(2L)
+                .build();
+
+            // when
+            ProfileResponse response = myPageService.updateProfile(profileRequest);
+
+            // then
+            assertThat(response.nickname()).isEqualTo(newNickname);
+            assertThat(response.imgUrl()).isEqualTo("www.example2.com");
+        }
+
+        @DisplayName("닉네임만 수정한다.")
+        @Test
+        @WithCustomMockUser
+        void updateNickname() {
+            // given
+            ProfileRequest profileRequest = ProfileRequest.builder()
+                .nickname(newNickname)
+                .build();
+
+            // when
+            ProfileResponse response = myPageService.updateProfile(profileRequest);
+
+            // then
+            assertThat(response.nickname()).isEqualTo(newNickname);
+            assertThat(response.imgUrl()).isEqualTo("www.example1.com");
+        }
+
+        @DisplayName("프로필 이미지만 수정한다.")
+        @Test
+        @WithCustomMockUser
+        void updateProfileImage() {
+            // given
+            ProfileRequest profileRequest = ProfileRequest.builder()
+                .profileImg(2L)
+                .build();
+
+            // when
+            ProfileResponse response = myPageService.updateProfile(profileRequest);
+
+            // then
+            assertThat(response.nickname()).isEqualTo("1번유저");
+            assertThat(response.imgUrl()).isEqualTo("www.example2.com");
+        }
     }
 }
