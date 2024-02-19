c79050bf0c0b9d9348f59bb12839325a23ba1a07
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-20 03:21:18 +0900

    deploy: gift_box, receiver 테이블에 soft delete 컬럼 추가하는 flyway 스크립트 작성

---
+++ packy-domain/src/main/resources/db/migration/V29__add_delete_status_in_gift_box_and_receiver_table.sql
@@ -0,0 +1,8 @@
+-- gift_box 테이블에 sender_deleted 컬럼 추가
+alter table gift_box
+add column sender_deleted bit not null default false;
+
+-- receiver 테이블에 status 컬럼 추가
+alter table receiver
+add column status enum('RECEIVED', 'DELETED') not null default 'RECEIVED';
+´
