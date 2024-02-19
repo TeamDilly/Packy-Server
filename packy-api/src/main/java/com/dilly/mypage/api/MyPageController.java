d071375c3c5f90eacebaae2d9090bcd74deec9ea
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-02-20 01:15:48 +0900

    feat: 나의 프로필 수정 API 구현 (#168)
    * feat: 나의 프로필 수정 API 구현
    
    * test: 나의 프로필 수정 API Controller, Service 테스트 코드 작성
    
    * docs: 나의 프로필 수정 API Swagger 설명 추가

--- packy-api/src/main/java/com/dilly/mypage/api/MyPageController.java
+++ packy-api/src/main/java/com/dilly/mypage/api/MyPageController.java
@@ -2,11 +2,15 @@ package com.dilly.mypage.api;
 
 import com.dilly.global.response.DataResponseDto;
 import com.dilly.mypage.application.MyPageService;
+import com.dilly.mypage.dto.request.ProfileRequest;
 import com.dilly.mypage.dto.response.ProfileResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,12 @@ public class MyPageController {
     @GetMapping("/profile")
     public DataResponseDto<ProfileResponse> getProfile() {
         return DataResponseDto.from(myPageService.getProfile());
+    }
+
+    @Operation(summary = "나의 프로필 수정", description = "수정할 column만 request body에 담아서 요청해주세요.")
+    @PatchMapping("/profile")
+    public DataResponseDto<ProfileResponse> updateProfile(
+        @RequestBody @Valid ProfileRequest profileRequest) {
+        return DataResponseDto.from(myPageService.updateProfile(profileRequest));
     }
 }
