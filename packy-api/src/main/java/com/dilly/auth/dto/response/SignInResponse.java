93a9efdd7adac0c4e1aa34bb6065649737825928
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-02-20 01:22:30 +0900

    fix: 로그인 API 응답값에 닉네임 추가 (#169)

--- packy-api/src/main/java/com/dilly/auth/dto/response/SignInResponse.java
+++ packy-api/src/main/java/com/dilly/auth/dto/response/SignInResponse.java
@@ -2,11 +2,32 @@ package com.dilly.auth.dto.response;
 
 import com.dilly.jwt.dto.JwtResponse;
 import com.dilly.member.domain.Status;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record SignInResponse(
+	@Schema(example = "NOT_REGISTERED")
 	Status status,
+	@Schema(example = "짱제이")
+	String nickname,
 	JwtResponse tokenInfo
 ) {
+
+	public static SignInResponse from(Status status, JwtResponse tokenInfo) {
+		return SignInResponse.builder()
+			.status(status)
+			.tokenInfo(tokenInfo)
+			.build();
+	}
+
+	public static SignInResponse from(Status status, String nickname, JwtResponse tokenInfo) {
+		return SignInResponse.builder()
+			.status(status)
+			.nickname(nickname)
+			.tokenInfo(tokenInfo)
+			.build();
+	}
 }
