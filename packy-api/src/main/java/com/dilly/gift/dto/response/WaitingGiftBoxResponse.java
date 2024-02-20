704c05ba4178c913288e058ca24134f6df3a0fe7
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-21 01:12:42 +0900

    docs: WaitingGiftBoxResponse Swagger 예시 추가

--- packy-api/src/main/java/com/dilly/gift/dto/response/WaitingGiftBoxResponse.java
+++ packy-api/src/main/java/com/dilly/gift/dto/response/WaitingGiftBoxResponse.java
@@ -1,15 +1,21 @@
 package com.dilly.gift.dto.response;
 
 import com.dilly.gift.domain.giftbox.GiftBox;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
 public record WaitingGiftBoxResponse(
+    @Schema(example = "1")
     Long id,
+    @Schema(example = "선물박스 이름")
     String name,
+    @Schema(example = "받는 사람 이름")
     String receiverName,
+    @Schema(example = "2024-01-01T00:00:00.0000")
     LocalDateTime createdAt,
+    @Schema(example = "www.example.com")
     String smallImgUrl
 ) {
 
