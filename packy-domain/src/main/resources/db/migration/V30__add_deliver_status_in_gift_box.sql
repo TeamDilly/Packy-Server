745b5925b323925f471fc6494f6fed07880ef792
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-22 00:19:21 +0900

    deploy: 기존에 만들어진 선물박스를 DELIVERED 처리할 때 created_at을 데모데이 이전으로 명시적으로 기입

--- packy-domain/src/main/resources/db/migration/V30__add_deliver_status_in_gift_box.sql
+++ packy-domain/src/main/resources/db/migration/V30__add_deliver_status_in_gift_box.sql
@@ -5,4 +5,4 @@ add column deliver_status enum('WAITING', 'DELIVERED') not null default 'WAITING
 -- 기존에 만들어진 박스는 DELIVERED 처리
 update gift_box
 set deliver_status = 'DELIVERED'
-where created_at < NOW();
+where created_at < '2024-02-24 00:00:00';
