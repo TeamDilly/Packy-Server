84531b3c17821d3bf720336997a060c054d7dab8
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-23 01:06:19 +0900

    refactor: Random 대신 SecureRandom 사용

--- packy-api/src/main/java/com/dilly/global/utils/generator/RandomBooleanGenerator.java
+++ packy-api/src/main/java/com/dilly/global/utils/generator/RandomBooleanGenerator.java
@@ -1,10 +1,21 @@
 package com.dilly.global.utils.generator;
 
-import java.util.Random;
+import com.dilly.exception.ErrorCode;
+import com.dilly.exception.internalserver.InternalServerException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 public class RandomBooleanGenerator {
 
-    private static Random random = new Random();
+    private static final SecureRandom random;
+
+    static {
+        try {
+            random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            throw new InternalServerException(ErrorCode.INTERNAL_ERROR);
+        }
+    }
 
     public static boolean generate() {
         return random.nextBoolean();
