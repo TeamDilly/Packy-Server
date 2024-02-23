ddffaed1c7d007cd3b6140267f1b70d78d80a107
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-02-23 22:15:31 +0900

    remove: 패키의 선물박스를 받은 날짜 수정에 따라 사용되지 않는 코드 제거

--- packy-domain/src/main/java/com/dilly/gift/dao/querydsl/GiftBoxQueryRepository.java
+++ packy-domain/src/main/java/com/dilly/gift/dao/querydsl/GiftBoxQueryRepository.java
@@ -1,16 +1,13 @@
 package com.dilly.gift.dao.querydsl;
 
 import static com.dilly.gift.domain.giftbox.QGiftBox.giftBox;
-import static com.dilly.gift.domain.giftbox.admin.QAdminGiftBox.adminGiftBox;
 import static com.dilly.gift.domain.receiver.QReceiver.receiver;
 
 import com.dilly.gift.domain.giftbox.DeliverStatus;
 import com.dilly.gift.domain.giftbox.GiftBox;
-import com.dilly.gift.domain.giftbox.admin.AdminType;
 import com.dilly.gift.domain.receiver.ReceiverStatus;
 import com.dilly.member.domain.Member;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -95,15 +92,11 @@ public class GiftBoxQueryRepository {
         return jpaQueryFactory.select(giftBox)
             .from(receiver)
             .join(receiver.giftBox, giftBox)
-            .leftJoin(giftBox.adminGiftBox, adminGiftBox)
             .where(
                 ltReceivedDate(lastGiftBoxDate),
                 receiver.member.eq(member),
                 receiver.status.eq(ReceiverStatus.RECEIVED))
             .orderBy(
-                new CaseBuilder()
-                    .when(adminGiftBox.adminType.eq(AdminType.ONBOARDING)).then(1)
-                    .otherwise(0).asc(),
                 receiver.createdAt.desc()
             )
             .limit(pageable.getPageSize() + 1L)
