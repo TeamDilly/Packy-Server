3e223c7adc220a1fad8a5ef151601645df231a85
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-02-23 13:12:11 +0900

    fix: 메인화면에 선물박스를 랜덤하게 띄우는 로직 제거 (#190)
    * fix: 메인화면에 선물박스를 랜덤하게 띄우는 로직 제거
    
    * deploy: release drafter 액션 주석 처리

--- packy-domain/src/main/java/com/dilly/admin/adaptor/AdminGiftBoxReader.java
+++ packy-domain/src/main/java/com/dilly/admin/adaptor/AdminGiftBoxReader.java
@@ -3,6 +3,7 @@ package com.dilly.admin.adaptor;
 import com.dilly.gift.dao.AdminGiftBoxRepository;
 import com.dilly.gift.domain.giftbox.admin.AdminGiftBox;
 import com.dilly.gift.domain.giftbox.admin.AdminType;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,7 @@ public class AdminGiftBoxReader {
 
     private final AdminGiftBoxRepository adminGiftBoxRepository;
 
-    public AdminGiftBox findByAdminType(AdminType adminType) {
+    public Optional<AdminGiftBox> findByAdminType(AdminType adminType) {
         return adminGiftBoxRepository.findByAdminType(adminType);
     }
 }
--- packy-domain/src/main/java/com/dilly/gift/dao/AdminGiftBoxRepository.java
+++ packy-domain/src/main/java/com/dilly/gift/dao/AdminGiftBoxRepository.java
@@ -2,9 +2,10 @@ package com.dilly.gift.dao;
 
 import com.dilly.gift.domain.giftbox.admin.AdminGiftBox;
 import com.dilly.gift.domain.giftbox.admin.AdminType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminGiftBoxRepository extends JpaRepository<AdminGiftBox, Long> {
 
-    AdminGiftBox findByAdminType(AdminType adminType);
+    Optional<AdminGiftBox> findByAdminType(AdminType adminType);
 }
