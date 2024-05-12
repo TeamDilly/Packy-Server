fbd50bfe7a14f32a26965eef534785571fafa6ac
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-05-12 15:41:30 +0900

    feat: 슬랙 회원가입 알림 봇 구현 (#220)
    * feat: 슬랙 회원가입 알림 봇 구현
    
    * fix: SonarLint를 반영하여 코드 리팩토링

--- packy-domain/src/main/java/com/dilly/member/MemberRepository.java
+++ packy-domain/src/main/java/com/dilly/member/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.dilly.member;
 
 import com.dilly.member.domain.Member;
+import com.dilly.member.domain.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Long countByStatus(Status status);
 }
