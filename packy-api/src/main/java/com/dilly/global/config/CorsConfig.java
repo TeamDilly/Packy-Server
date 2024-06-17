d69f655cae2f3e947b4f6093ed680dfdb7265a9e
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-06-17 23:05:46 +0900

    fix: CORS origin 추가 (#229)
    * fix: CORS origin 추가
    
    * style: 누락된 세미콜론 추가

--- packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
+++ packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedMethods("GET")
-            .allowedOrigins("https://packyforyou.com");
+            .allowedOrigins("https://packyforyou.com")
+            .allowedOrigins("https://dev.packyforyou.com");
     }
 }
