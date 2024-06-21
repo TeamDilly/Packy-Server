f59ce3f160f0f76f55db8029ff2040616a3dd349
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-06-22 00:01:47 +0900

    fix: CorsConfig allowedMethods 수정

--- packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
+++ packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("GET")
+            .allowedMethods("*")
             .allowedOrigins("https://packyforyou.com", "https://dev.packyforyou.com");
     }
 }
