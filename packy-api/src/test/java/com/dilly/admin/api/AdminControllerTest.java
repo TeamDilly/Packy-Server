c2273b1b9f3b73d21e62dcf901fc0fbf2d5d0fc1
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-06-23 17:29:01 +0900

    test: 스티커 조회, 유튜브 링크 유효성 검사 API Controller 테스트 코드 추가

--- packy-api/src/test/java/com/dilly/admin/api/AdminControllerTest.java
+++ packy-api/src/test/java/com/dilly/admin/api/AdminControllerTest.java
@@ -11,17 +11,23 @@ import com.dilly.admin.dto.response.BoxImgResponse;
 import com.dilly.admin.dto.response.ImgResponse;
 import com.dilly.admin.dto.response.MusicResponse;
 import com.dilly.admin.dto.response.SettingResponse;
+import com.dilly.dto.response.YoutubeUrlValidationResponse;
 import com.dilly.gift.dto.response.EnvelopeListResponse;
 import com.dilly.gift.dto.response.EnvelopePaperResponse;
 import com.dilly.global.ControllerTestSupport;
 import com.dilly.global.WithCustomMockUser;
 import com.dilly.global.fixture.DtoFixture;
+import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junitpioneer.jupiter.RestoreSystemProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
 class AdminControllerTest extends ControllerTestSupport {
 
@@ -143,6 +149,58 @@ class AdminControllerTest extends ControllerTestSupport {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.data").isArray())
 			.andExpect(jsonPath("$.data", hasSize(2)));
+	}
+
+	// TODO: size와 lastStickerId에 따라 다른 결과를 반환하는지 확인은 Controller? Service?
+	@DisplayName("페이지네이션으로 스티커를 조회한다.")
+	@Test
+	@WithCustomMockUser(id = MEMBER_ID)
+	void getFirst10() throws Exception {
+		// given
+		// TODO: 테스트 코드에서 for문을 사용해도 되는가?
+		List<ImgResponse> imgResponses = new ArrayList<>();
+		long pageDefaultSize = 10;
+		for (long i = 1; i <= pageDefaultSize; i++) {
+			imgResponses.add(DtoFixture.imgResponse(i, i));
+		}
+
+		Pageable pageable = PageRequest.of(0, 10);
+		Slice<ImgResponse> imgResponseSlice = new SliceImpl<>(imgResponses, pageable, false);
+
+		given(adminService.getStickers(null, pageable))
+			.willReturn(imgResponseSlice);
+
+		// when // then
+		mockMvc.perform(
+				get(baseUrl + "/admin/design/stickers")
+			)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.content").isArray())
+			.andExpect(jsonPath("$.data.content", hasSize(10)))
+			.andExpect(jsonPath("$.data.first").value(true))
+			.andExpect(jsonPath("$.data.last").value(true));
+	}
+
+	@DisplayName("유튜브 링크 유효성 검사 결과를 반환한다.")
+	@Test
+	@WithCustomMockUser(id = MEMBER_ID)
+	void validateYoutubeUrl() throws Exception {
+		// given
+		String url = "https://www.youtube.com/watch?v=1234";
+		YoutubeUrlValidationResponse validationResponse = YoutubeUrlValidationResponse.builder()
+			.status(true).build();
+
+		given(youtubeService.validateYoutubeUrl(url)).willReturn(validationResponse);
+
+		// when // then
+		mockMvc.perform(
+				get(baseUrl + "/admin/youtube")
+					.param("url", url)
+			)
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.status").value(true));
 	}
 
 	@DisplayName("설정 링크를 조회한다.")
