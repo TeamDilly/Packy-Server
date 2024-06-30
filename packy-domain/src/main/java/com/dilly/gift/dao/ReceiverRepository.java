c83747e1213b2beb3b741d555369150caacee4af
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-07-01 01:40:23 +0900

    remove: 사용하지 않는 함수 제거

--- packy-domain/src/main/java/com/dilly/gift/dao/ReceiverRepository.java
+++ packy-domain/src/main/java/com/dilly/gift/dao/ReceiverRepository.java
@@ -7,9 +7,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReceiverRepository extends JpaRepository<Receiver, Long> {
-
-    Boolean existsByGiftBox(GiftBox giftBox);
-
+    
     Long countByGiftBox(GiftBox giftBox);
 
     List<Receiver> findByGiftBox(GiftBox giftBox);
