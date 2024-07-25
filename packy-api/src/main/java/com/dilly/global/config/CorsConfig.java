33437cb88a518971dbc02392731e254d520917e6
leeeeeyeon <leeeeeyeonn@gmail.com>
2024-07-25 18:29:03 +0900

    fix: CORS allowedMethods 메소드 추가

--- packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
+++ packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedMethods("HEAD", "GET", "OPTIONS")
+            .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
             .allowedOrigins("https://packyforyou.com", "https://dev.packyforyou.com");
     }
 }
