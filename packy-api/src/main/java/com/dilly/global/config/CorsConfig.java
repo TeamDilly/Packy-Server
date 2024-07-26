660a53809e5a6f2827c224bfca65a19978b5ff42
Jeongyeon Lee <leeeee_yeon@naver.com>
2024-07-26 20:25:12 +0900

    fix: allowedHeaders 와일드 카드 추가 (#251)

--- packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
+++ packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
@@ -10,6 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+            .allowedHeaders("*")
             .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
             .allowedOrigins("https://packyforyou.com", "https://dev.packyforyou.com");
     }
