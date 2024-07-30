ff0d58c41cd39dbe6d4836cf788dc1de3d2b323c
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-07-30 18:09:22 +0900

    fix: allowedOrigins를 설정 파일에서 불러오는 방식으로 변경

--- packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
+++ packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.dilly.global.config;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,13 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowedOrigins}")
+    private List<String> allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
             .allowedHeaders("*")
             .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
-            .allowedOrigins("https://packyforyou.com", "https://dev.packyforyou.com",
-                "https://dev.packyforyou.shop")
+            .allowedOrigins(allowedOrigins.toArray(new String[0]))
             .maxAge(3600);
     }
 }
