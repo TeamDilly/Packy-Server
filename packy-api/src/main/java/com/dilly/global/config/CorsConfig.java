bebf618d15be46aee5071e75a2e4201f53fb17ed
Lee Jeong Yeon <leeeeeyeonn@gmail.com>
2024-07-30 18:16:50 +0900

    fix: local일 때 CorsConfig 비활성화

--- packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
+++ packy-api/src/main/java/com/dilly/global/config/CorsConfig.java
@@ -3,10 +3,12 @@ package com.dilly.global.config;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile({"dev", "prod"})
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${cors.allowedOrigins}")
